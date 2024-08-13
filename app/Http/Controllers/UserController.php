ad7783b05df6be50e7aa4ffec705323835ab4794
Jawad Ahmed <jodye888@gmail.com>
2024-08-13 14:42:03 +0500

    login work done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     //
+    public function showCorrectHomepage(){
+        if(auth()->check()){
+            return view("homePage_feed");
+        }else{ 
+            return view("homePage");
+        }
+    }
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            "loginusername" => "required",
+            "loginpassword" => "required"
+        ]);
+        if(auth()->attempt(["username" => $incomingFields["loginusername"], "password" => $incomingFields["loginpassword"]])){
+            $request->session()->regenerate();
+            return "Congrats!!!1";
+        }else{
+            return "sorry";
+        }
+    }
+
     public function register(Request $request){
         $incomingFields = $request->validate([
-            "username" => "required",
-            "email" => "required", 
-            "password" => "required"
+            "username" => ["required", "min:3", "max:20", Rule::unique("users","username")],
+            "email" => ["required","email", Rule::unique("users","email")], 
+            "password" => ["required","min:3","confirmed"]
         ]);
+
+        $incomingFields["password"] = bcrypt($incomingFields["password"]);
 
         User::create($incomingFields);
         return "hello from register function";
